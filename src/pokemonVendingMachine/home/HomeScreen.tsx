94961f5e0b7456d9af63d74b2dfb90b10a1ad2d2
shiyoohashi <alicehecate666@gmail.com>
2023-05-31 19:10:38 +0900

    add:ボタンを押したらid name typeを表示

--- src/pokemonVendingMachine/home/HomeScreen.tsx
+++ src/pokemonVendingMachine/home/HomeScreen.tsx
@@ -1,10 +1,32 @@
 import styles from './HomeScreen.module.scss'
 import Contents from '../../Contents'
+import {useState} from "react";
+
+
+type PokeObj = {
+    id: string,
+    name: string,
+    type: string,
+}
 
 export default function HomeScreen() {
+    const [pokeObj, setPokeObj] = useState<PokeObj>({id: '', name: '', type: ''})
+
+    function randomSelectPokemon() {
+        setPokeObj({
+            id: '#25',
+            name: 'ピカチュウ',
+            type: 'カミナリ'
+        })
+    }
+
     return (
         <Contents>
             <h1 className={styles.title}>Pokemon Vending Machine</h1>
+            <p onClick={randomSelectPokemon}>ポケモンゲットだぜ！</p>
+            <p>{pokeObj.id}</p>
+            <p>{pokeObj.name}</p>
+            <p>{pokeObj.type}</p>
         </Contents>
     )
 }
--- src/tests/home/HomeScreen.test.tsx
+++ src/tests/home/HomeScreen.test.tsx
@@ -1,6 +1,7 @@
-import {screen} from '@testing-library/react'
+import {act, screen, waitFor} from '@testing-library/react'
 import renderApplication from '../RenderApplication'
 import AppPropsBuilder from '../../AppPropsBuilder'
+import userEvent from "@testing-library/user-event";
 
 describe('', () => {
     test('購入画面のページタイトルが表示されている', async () => {
@@ -8,5 +9,49 @@ describe('', () => {
 
 
         expect(screen.getByText('Pokemon Vending Machine')).toBeInTheDocument()
+    })
+
+    test('ポケモンをゲットするボタンがあること', async () => {
+        await renderApplication('/', (new AppPropsBuilder()).build())
+
+        expect(screen.getByText('ポケモンゲットだぜ！')).toBeInTheDocument()
+    })
+
+    describe('ボタンを押した時', () => {
+        const clickButton = () => {
+            const buttonElement = screen.getByText('ポケモンゲットだぜ！')
+            userEvent.click(buttonElement)
+
+        }
+        test('ボタンを押したらIDが表示されること', async () => {
+
+            await renderApplication('/', (new AppPropsBuilder()).build())
+
+            clickButton()
+
+            await waitFor(() => {
+                expect(screen.getByText('#25')).toBeInTheDocument()
+            })
+        })
+
+        test('ボタンを押したら名前が表示されること', async () => {
+            await renderApplication('/', (new AppPropsBuilder()).build())
+
+            clickButton()
+
+            await waitFor(() => {
+                expect(screen.getByText('ピカチュウ')).toBeInTheDocument()
+            })
+        })
+
+        test('ボタンを押したらタイプが表示されること', async () => {
+            await renderApplication('/', (new AppPropsBuilder()).build())
+
+            clickButton()
+
+            await waitFor(() => {
+                expect(screen.getByText('カミナリ')).toBeInTheDocument()
+            })
+        })
     })
 })
